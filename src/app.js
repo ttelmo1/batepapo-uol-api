96da2e10f7798fd924d4cce28a1ca14e381a38ff
ttelmo1 <telmobsj@gmail.com>
2023-01-10 12:29:48 -0300

    post de participantes criado

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import { MongoClient } from 'mongodb';
+import Joi from 'joi';
+import dayjs from 'dayjs'; 
+import 'dayjs/locale/pt-br.js';
+
+const now = dayjs.locale('pt-br')
+
+const schema = Joi.object({
+    name: Joi.string()
+});
+
 
 
 dotenv.config();
@@ -9,6 +20,41 @@ console.log(process.env.PORT);
 const app = express();
 app.use(express.json());
 app.use(cors());
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+mongoClient.connect();
+const db = mongoClient.db();
+
+
+async function getUsers() {
+    const users = await db.collection("participants").find().toArray();
+    return users;
+}
+
+
+app.post("/participants" , async (req, res) => {
+    const user = req.body;
+
+    try {
+        const value = await schema.validateAsync({name : user.name});
+        const users = await getUsers();
+
+
+        const userExists = users.find((u) => u.name === user.name);
+        if(userExists) {
+            return res.sendStatus(409);
+        }
+        
+        user.lastStatus = Date.now();
+        await db.collection("participants").insertOne(user);
+        res.sendStatus(201);
+    }
+    catch (error) {
+        res.status(422).send(error);
+    }
+
+
+    
+})
 
 
 
