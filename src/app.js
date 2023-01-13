7402d041cc895513582b53329f28d075589f3a68
ttelmo1 <telmobsj@gmail.com>
2023-01-13 15:06:03 -0300

    tratar post invalido

--- src/app.js
+++ src/app.js
@@ -40,12 +40,16 @@ app.post("/participants" , async (req, res) => {
         const user = req.body;
         const users = await getUsers();
         await schema.validateAsync({name : user.name});
+        if(user.name === "") {
+            return res.sendStatus(422);
+        }
+        
 
         const userExists = users.find((u) => u.name === user.name);
         if(userExists) {
             return res.sendStatus(409);
         }
-
+        
         user.lastStatus = Date.now();
         await db.collection("participants").insertOne(user);
         res.sendStatus(201);
@@ -60,16 +64,16 @@ app.get("/participants" , async (req, res) => {
     res.send(await getUsers());
 })
 
-setInterval(async () => {
-    const users = await getUsers();
-    const now = Date.now();
-    const inactiveUsers = users.filter((u) => now - u.lastStatus > 10000);
-    if(inactiveUsers.length > 0) {
-        await db.collection("participants").deleteMany({
-            name: { $in: inactiveUsers.map((u) => u.name) }
-        });
-    }
-}, 15000);
+// setInterval(async () => {
+//     const users = await getUsers();
+//     const now = Date.now();
+//     const inactiveUsers = users.filter((u) => now - u.lastStatus > 10000);
+//     if(inactiveUsers.length > 0) {
+//         await db.collection("participants").deleteMany({
+//             name: { $in: inactiveUsers.map((u) => u.name) }
+//         });
+//     }
+// }, 15000);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
