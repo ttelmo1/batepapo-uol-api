61e707f4a3fc75065153633ec5f7d8bb14da6c20
ttelmo1 <telmobsj@gmail.com>
2023-01-15 15:14:06 -0300

    update de mensagens

--- src/app.js
+++ src/app.js
@@ -208,7 +208,25 @@ app.delete("/messages/:id", async (req, res) => {
     }
 });
 
-    
+app.put("/messages/:id", async (req, res) => {
+    const id = req.params.id;
+  
+    try{
+        const message = await db.collection("messages").findOne({ _id: ObjectId(id) });
+        if (!message) {
+            return res.sendStatus(404);
+        }
+        if (message.from !== user) {
+            return res.sendStatus(401);
+        }
+        await db.collection("messages").updateOne({ _id: ObjectId(id) }, { $set: { text: req.body.text } });
+        res.sendStatus(200);
+    }
+    catch (error) {
+        res.sendStatus(500);
+    }
+});
+   
 
 
 setInterval(async () => {
